4a5b61706e45e46432616192366d430b56d582fc
unknown <ricemills999@gmail.com>
2025-12-21 20:28:00 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ 
     error: 'Internal Server Error',
-    message: process.env.NODE_ENV === 'development' ? err.message : 'Something went wrong!'
+    message: err.message || 'Something went wrong!' 
   });
 });
 
